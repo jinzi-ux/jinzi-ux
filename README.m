df0f7ccbd35e75f3efd754eac0abf1c5019759ba
jinzi-ux <144139966+jinzi-ux@users.noreply.github.com>
2023-09-05 18:46:48 +0400

    create ios.16.3

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 ios.16.1 I’m @jinzi-ux
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning coding..
+- 💞️ I’m looking to collaborate on codespaces...
+- 📫 How to reach me web ...
+
+<!---
+jinzi-ux/jinzi-ux is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
